7d01081541e64b6b140e857b255c589f9beb31dd
auliannisafjra <auliarara03@gmail.com>
2024-04-23 22:50:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'list-menu'], function () {
+    Route::get('/', function () {
+        return 'Ini adalah halaman yang menampilkan semua menu di Amandemy Cafe.';
+    });
+
+    Route::get('/get-all-data', function () {
+        return response()->json([
+            'info' => 'Data berhasil didapatkan',
+            'data' => [
+                [
+                    'makanan' => 'Kebab',
+                    'harga' => 15000
+                ],
+                [
+                    'makanan' => 'Mie Ayam',
+                    'harga' => 18000
+                ],
+                [
+                    'makanan' => 'Martabak Mesir',
+                    'harga' => 20000
+                ]
+            ]
+        ]);
+    });
+
+    Route::get('/{makanan}/{harga}', function ($makanan, $harga) {
+        $menu = "Anda memilih makanan $makanan dengan harga $harga";
+        return $menu;
+    });
+});
